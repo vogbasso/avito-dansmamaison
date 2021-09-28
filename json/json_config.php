4cefb07591f7f72d81bf1c562ca33dcb955cad98
vogbasso <vogelpaolo@gmail.com>
2021-09-28 17:38:39 +0100

    Création du file json qui contient la data

--- json/json_config.php
+++ json/json_config.php
@@ -6,7 +6,7 @@ $jso = json_encode($listproducts, JSON_PRETTY_PRINT);
 echo json_last_error_msg();
 echo "LA PARTIE DU JSON \n";
 var_dump($jso);
-echo $jso;
 
 require('json_file.php');
+
 ?>
--- json/json_file.php
+++ json/json_file.php
@@ -1,5 +1,10 @@
 <?php
 
+// ouverture du fichier JSON sur le serveur.
 
+$fichierJson = fopen("mamaison.json","w") or die("Impossible de trouver ce fichier");
+fwrite($fichierJson,$jso);
+fclose($fichierJson);
 
+echo "on est dans le fichier de gestion du file avito";
 ?>
