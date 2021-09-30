8a0aeb61a7ea25b60c2710a68208d586cdb2bf74
vogbasso <vogelpaolo@gmail.com>
2021-09-30 13:47:17 +0100

    Réécriture du code du fichier request en prenant en compte l'absence d'url et en retirant de la liste les articles qui non pas l'url car tous les articles publié doivent avoi une image. aussi j'ai Ajouté la status en stock ou pas de l'article

--- models/avito_product.php
+++ models/avito_product.php
@@ -5,16 +5,19 @@ class avito_product{
     
     public $id; //identifiant sku or ugs obligatoire
     public $title; //titre de l'article required max 50 caractères obligatoire
+    public $description=""; // description du produit
     public $published = true; //publier valeur par défaut TRUE
     public $type = "sell"; //Type rent ou sell default sell
     public $category = 3020;
     public $price; // prix de l'article (entre prix promo et prix normal)
-    public $phone = "0522275438"; // téléphone du shop par défaut;
+    public $phone = "0520163443"; // téléphone du shop par défaut;
     public $city = 5; //par défaut casablanca code 5
     public $area = null;  //recommander AREA IDS
     public $itemcondition = 0; // par défaut 0 pour article neuf
     public $images = array(); // tableau des url des images if supérieur à 8 les 8 premiers seront chargé
-    public $description=""; // description du produit
+    public $inStockStatus;
+    
+    public $delivery = true; //option de livraison par défaut à true;
 }
 
 echo "Avito product models Class : avito_product.php \n";
--- mysql/mysql_request.php
+++ mysql/mysql_request.php
@@ -11,117 +11,76 @@ $listproducts = array();
 
 /*** PREMIERE REQUETE Récupère la liste des produits ***/
 //requi = "select * from wp_wc_product_meta_lookup wwpml inner join wp_posts wp on wwpml.product_id = wp.ID where wwpml.sku <> '' and wwpml.sku is not null and wp.post_status = 'publish' limit 10;";
-$req = $conn->prepare("select * from wp_wc_product_meta_lookup wwpml inner join wp_posts wp on wwpml.product_id = wp.ID where wwpml.sku <> '' and wwpml.sku is not null and wp.post_status = 'publish' limit 10;");
+$req = $conn->prepare("select * from wp_wc_product_meta_lookup wwpml inner join wp_posts wp on wwpml.product_id = wp.ID where wwpml.sku <> '' and wwpml.sku is not null and wp.post_status = 'publish';");
 //$req = $conn->prepare("select wp_posts.ID, wp_posts.post_title, wp_pos from wp_posts where wp_posts.post_type='product' or wp_posts.post_type='product_variation' order by wp_posts.ID limit 10;");
 //$reponse = $conn->query('select wp_posts.ID,wp_post_skr from wp_posts,wp_wc_product_meta_lookup AS wc_product where wp_posts.ID = wc_product.product_ID limit 20');
 $req->execute();
 //$req = avito_request($requi,$conn);
 //print_r($req);
-/*** BOUCLE POUR APPLER TOUS LES PRODUITS DE LA BASE DE DONNER ***/
-while($donne = $req->fetch()){
-   $id_produit = $donne['ID'];
-   //echo "l'identifiant du produit $id_produit \n";
-   $avitoProduct = new avito_product();
-   $avitoProduct->id = $donne['sku']; //sku à la place de ID pour AVITO
-   $avitoProduct->title = $donne['post_title']; //Titre pour post_title pour Avito utf8_encode($donne['post_title']);
-   $avitoProduct->price = (int) min($donne['min_price'],$donne['max_price']);
-   $avitoProduct->description = (string) $donne['post_content'];
-   //echo "Voici la valeur de publish ".$donne['post_status'];
-   $avitoProduct->published = $donne['post_status'] == 'publish' ? true : false;
 
+
+
+while($donne = $req->fetch()){ /*** BOUCLE POUR APPLER TOUS LES PRODUITS DE LA BASE DE DONNER ***/
+
+   $id_produit = $donne['ID']; // affection de l'id du produit afin de récupérer les GUID des urls
+   
+   $avitoProduct = new avito_product(); // instantiation d'une variable Avito product
+
+   $avitoProduct->id = $donne['sku']; //sku à la place de ID pour AVITO
+   
+   $avitoProduct->title = $donne['post_title']; //Titre pour post_title pour Avito utf8_encode($donne['post_title']);
+   
+   $avitoProduct->price = (int) min($donne['min_price'],$donne['max_price']); // prix de vente est le prix minimum entre le prix max et le prix min
+   
+   $avitoProduct->description = (string) $donne['post_content']; // s'assurer que c'est converti en chaîne de caractère
+
+   $avitoProduct->published = $donne['post_status'] == 'publish' ? true : false; /// définir le status a publié ou non
+
+   $avitoProduct->inStockStatus = $donne['stock_status'] == 'instock' ? true : false; // définir le status du stock en Stock True, outofstock false 
 
    /***  Début de la  deuxième requête dans le but d'utiliser l'id pour récupérer cette informations ***/
-     // $req2 = $conn->prepare("select guid from wp_posts wp2 inner join (select meta_value from wp_postmeta wp where wp.post_id = :identifiant and meta_key = '_thumbnail_id') as meta_thumbnail on wp2.ID = meta_value ;");
+   $req2 = $conn->prepare("select wp.post_id, wp.meta_key, wp.meta_value from wp_postmeta wp where (wp.meta_key = '_thumbnail_id' or wp.meta_key = '_product_image_gallery') and wp.post_id = :identifiant order by wp.meta_key desc limit 10;");
+   $req2->execute(array("identifiant"=>$id_produit));
+   // $req2 = $conn->prepare("select guid from wp_posts wp2 inner join (select meta_value from wp_postmeta wp where wp.post_id = :identifiant and meta_key = '_thumbnail_id') as meta_thumbnail on wp2.ID = meta_value ;");
       
-      $req2 = $conn->prepare("select wp.post_id, wp.meta_key, wp.meta_value from wp_postmeta wp where (wp.meta_key = '_thumbnail_id' or wp.meta_key = '_product_image_gallery') and wp.post_id = :identifiant limit 10;");
-      $req2->execute(array("identifiant"=>$id_produit));
-      
-      //print_r($req2);
-     // $i=1;
-      
-      while($url_images = $req2->fetch()){
+   while($url_images = $req2->fetch()){ /*** BOUCLE DANS LE BUT DE CONTROLER SI LES IMAGES SONT DISPONIBLE AFIN DE POUVOIR CREER LE LES PRODUITS AVITO***/
 
-        if ($url_images['meta_key'] and $url_images['meta_key']=='_thumbnail_id'){
-            $identifiant_thumbnail = $url_images['meta_value'];
-            $req3 = $conn->prepare("select guid from wp_posts wp where wp.ID = :identifiant_thumbnail;");
-            $req3->execute(array("identifiant_thumbnail"=>$identifiant_thumbnail));
-               while($guid_url = $req3->fetch()){
-                  if($guid_url['guid']){
-              //       echo $guid_url['guid']."\n";
-                     array_push($avitoProduct->images,array("url1"=>$guid_url['guid']));
-                  }
-               }
+      if( ($url_images['meta_key'] == '_thumbnail_id' or $url_images['meta_key']=='_product_image_gallery') and isset($url_images['meta_value']) and 0!= (int) $url_images['meta_value']){
+        echo "Trouveé et voici la id_produit $id_produit avec la meta KEY ".$url_images['meta_key']." meta value: ".$url_images['meta_value']."\n";
 
-            //echo $url_images['meta_key']."et la valeur ".$url_images['meta_value']." \n" ;
-        }elseif($url_images['meta_key'] and $url_images['meta_key']=='_product_image_gallery'){
-           //echo "produit galerie disponilvle \n";
-
-           $id_product_image_gallery = explode (",",$url_images['meta_value']);
-          // print_r ($id_product_image_gallery);
-
-           //$length = sizeof($id_product_image_gallery);
-           $i = 2;
-           foreach($id_product_image_gallery as $id_product){
-            //echo "Image product id : $id_product \3";
-            $req4 = $conn->prepare("select guid from wp_posts wp where wp.ID = :id_product_image;");
-            $req4->execute(array("id_product_image"=>$id_product));
-              //print_r($req4);
-            
-              while($guid_url = $req4->fetch()){
-                // echo "La valeur du l'url pour l'id : $id_product et son url : ".$guid_url['guid']."\n";
-                if($guid_url['guid']){
-
-                  array_push($avitoProduct->images,array("url$i"=>$guid_url['guid']));
-
-                }
-                $i = $i+1;
-              }
-           }
-           //echo "Longueur de l'array : $length \n";
-           
-           
-           //   while($guid_url = $req4->fetch()){
-                 //if($guid_url['guid']){
-                 //   echo $guid_url['guid']."\n";
-                 //   array_push($avitoProduct->images,array("url1"=>$guid_url['guid']));
-                // }
-            //  }
-
-          // echo $url_images['meta_key']."et la valeur ".$url_images['meta_value']." \n" ;
-
-        }
-//         }else{
-
-           // echo "missing \n";
-         //}
-       
-         //array_push($avitoProduct->images,array("url$i"=>$url_images['guid']));
+        if($url_images['meta_key']=='_thumbnail_id'){// contrôle pour les thumbnails
+         $identifiant_thumbnail = $url_images['meta_value'];
+         $req3 = $conn->prepare("select guid from wp_posts wp where wp.ID = :identifiant_thumbnail;");
+         $req3->execute(array("identifiant_thumbnail"=>$identifiant_thumbnail));
          
-        // $i = $i +1;
-         //echo "URL GUID ".$url_image['guid'];
-     
+         while($guid_url = $req3->fetch()){
+            array_push($avitoProduct->images,array("url1"=>$guid_url['guid']));
+         }
+      }elseif($url_images['meta_key']=='_product_image_gallery'){
+         $id_product_image_gallery = explode(",",$url_images['meta_value']);
+         $i = 2;
+         foreach($id_product_image_gallery as $id_gallery){
+            $req4 = $conn->prepare("select guid from wp_posts wp where wp.ID = :id_product_image;");
+            $req4->execute(array("id_product_image"=>$id_gallery));
+            while($guid_url = $req4->fetch()){
+               if($guid_url['guid']){
+                  array_push($avitoProduct->images,array("url$i"=>$guid_url['guid']));
+               }
+               $i = $i+1;
+            }
+         }
       }
-
-   /*** Début de la 3ème requêtes ***/
-   //$req3 = $conn->prepare();
-   //$req3->execute(array("identifiant"=>$id_produit));
-
-   //var_dump($req2);
-   array_push($listproducts,$avitoProduct);
-   
-   // print_r($listproducts);
-    //var_dump($donne);
-    // echo $donne['0']."\n";
-};
-
-//echo "étape \n";
-//print_r($listproducts);
-
-
-//echo string($reponse);
-//var_dump($reponse);
-//echo " \n";
-
-
-echo "dans la page request \n";
+      
+      }else{
+         continue;
+         echo "continuer le code à l'étape suivante \n";
+      }
+       // Ajouté le produit à la liste seulement quand il possède une image
+   }
+   if(sizeof($avitoProduct->images) != 0 ){
+      array_push($listproducts,$avitoProduct);
+   }
+   echo "le nombre de produit est ".sizeof($listproducts);
+   }
+   echo "dans la page request \n";
 ?>
